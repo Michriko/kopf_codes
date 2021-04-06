4abd534da6c247fcfe6236e1c72288216a07cb2b
= <=>
2021-04-07 00:05:19 +0200

    feat(contact): add contact component
    add github, linkedIn, xing and mail as contact possibilities

---
+++ src/components/contact.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-container fluid class="secondary text-center">
+    <h2>Kontakt</h2>
+    <p class="py-3">
+      Sie erreichen mich unter Xing, LinkedIn, oder per Mail. Ich freue mich auf
+      Ihre Nachricht.
+    </p>
+    <v-row class="py-3">
+      <v-col v-for="(contactType, index) in contactTypes" :key="index">
+        <v-btn
+          icon
+          x-large
+          :href="contactType.url"
+          target="_blank"
+          v-on:click="onButtonClick(contactType)"
+        >
+          <v-icon x-large>{{ contactType.icon }}</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+interface ContactType {
+  url?: string;
+  onClick?: () => void;
+  icon: string;
+}
+
+function sendMail(): void {
+  //prevent crawling the email address by bots
+  const scheme = "mailto://";
+  const domain = "kopf.codes";
+  const user = "info";
+
+  window.open(`${scheme}${user}@${domain}`);
+}
+
+@Component
+export default class Contact extends Vue {
+  contactTypes: ContactType[] = [
+    {
+      url: "https://www.xing.com/profile/Michael_Kopf29/",
+      icon: "mdi-xing",
+    },
+    {
+      url: "https://www.linkedin.com/in/michael-kopf-52018016b/",
+      icon: "mdi-linkedin",
+    },
+    {
+      url: "https://github.com/Michriko/",
+      icon: "mdi-github",
+    },
+    {
+      icon: "mdi-email",
+      onClick: sendMail,
+    },
+  ];
+
+  onButtonClick(contactType: ContactType): void {
+    if (!contactType.onClick) {
+      return;
+    }
+    contactType.onClick();
+  }
+}
+</script>
