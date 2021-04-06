4e2ddd8f9b7337758e6b12615328072fb7b09272
= <=>
2021-04-06 22:40:38 +0200

    feat(content): replace placeholder with first content

--- src/components/tech/techCard.vue
+++ src/components/tech/techCard.vue
@@ -1,8 +1,11 @@
 <template>
-  <v-card>
-    <v-card-title>{{ title }}</v-card-title>
+  <v-card class="py-4">
+    <v-icon x-large>{{ icon }}</v-icon>
+    <v-card-title class="block text-center">{{ title }}</v-card-title>
     <v-card-text>
-      <p>{{ description }}</p>
+      <v-chip v-for="(item, index) in description" :key="index" class="ma-1">
+        {{ item }}
+      </v-chip>
     </v-card-text>
   </v-card>
 </template>
@@ -12,7 +15,13 @@ import { Vue, Component, Prop } from "vue-property-decorator";
 
 @Component
 export default class TechCard extends Vue {
-  @Prop(String) title?: string;
-  @Prop(String) description?: string;
+  @Prop(String) title!: string;
+  @Prop(Array) description!: string[];
+  @Prop(String) icon?: string;
 }
 </script>
+<style lang="scss" scoped>
+.block {
+  display: block;
+}
+</style>
